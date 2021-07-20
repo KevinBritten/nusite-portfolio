fd99cb1dee694c04c34af311ff76f6f62fb534be
Fern knee <kevinjbritten@gmail.com>
2021-07-20 12:52:14 -0400

    added header and variable files

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li v-for="section in sections" :key="section">
+          <a :href="'#' + section.toLowerCase()">{{ section }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sections: [
+        "Hero",
+        "Mission Statement",
+        "Services",
+        "Work",
+        "Team",
+        "Blog",
+        "Contact",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  list-style-type: none;
+}
+li {
+  padding: 0 5px;
+}
+a {
+  display: block;
+}
+</style>
